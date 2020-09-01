3c62b95d5f6206d5f874204497536edd58ca9c67
Vibin <vibin.cr22@gmail.com>
2020-09-01 18:29:44 +0530

    menu done!

--- app/src/main/java/com/android/secedemoproj/LoginActivity.java
+++ app/src/main/java/com/android/secedemoproj/LoginActivity.java
@@ -36,13 +36,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if(Build.VERSION.SDK_INT>=21)
-        {
-            Window window=this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
-        }
 
         loginemail=findViewById(R.id.et_login_email);
         loginpassword=findViewById(R.id.et_loginpassword);
--- app/src/main/java/com/android/secedemoproj/SplashcreenActivity.java
+++ app/src/main/java/com/android/secedemoproj/SplashcreenActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class SplashcreenActivity extends AppCompatActivity {
@@ -19,6 +22,13 @@ public class SplashcreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
+        if(Build.VERSION.SDK_INT>=21)
+        {
+            Window window=this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
